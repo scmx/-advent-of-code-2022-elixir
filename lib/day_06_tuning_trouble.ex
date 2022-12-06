1ff977c38799436765befd23356d55ba9699054b
Albert Arvidsson <albert.arvidsson@gmail.com>
2022-12-06 08:26:15 +0100

    Refactor Day06TuningTrouble again

--- lib/day_06_tuning_trouble.ex
+++ lib/day_06_tuning_trouble.ex
@@ -1,32 +1,14 @@
 defmodule Adventofcode.Day06TuningTrouble do
   use Adventofcode
 
-  alias __MODULE__.{Parser, Part1}
-
-  def part_1(input), do: input |> Parser.parse |> Part1.solve(4)
-
-  def part_2(input), do: input |> Parser.parse |> Part1.solve(14)
-
-  defmodule Part1 do
-    def solve(chars, size), do: solve(chars, size, size)
-
-    def solve(chars = [_ | tail], size, received) do
-      chars
-      |> Enum.take(size)
-      |> MapSet.new
-      |> Enum.to_list
-      |> then(fn 
-        set when length(set) == size -> received
-        _ -> solve(tail, size, received + 1)
-      end)
-    end
+  def part_1(input, size \\ 4) do
+    input
+    |> String.trim()
+    |> String.to_charlist
+    |> Enum.chunk_every(size, 1)
+    |> Enum.find_index(& size == &1 |> Enum.uniq |> length)
+    |> Kernel.+(size)
   end
 
-  defmodule Parser do
-    def parse(input) do
-      input
-      |> String.trim()
-      |> String.to_charlist
-    end
-  end
+  def part_2(input), do: input |> part_1(14)
 end
